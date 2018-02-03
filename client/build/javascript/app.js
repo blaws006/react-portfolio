c5bcd769055e23af63c19bf1ca22302dea9b2f2f
blaws006 <blaws006>
2018-02-03 11:48:57 -0500

    deploying to heroku

---
+++ client/build/javascript/app.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    console.log("Connected")
+    $('.parallax').parallax();
+
+
+    var options = [{
+        selector: '#staggered-test',
+        offset: 400,
+        callback: function (el) {
+            Materialize.showStaggeredList($(el));
+        }
+    }];
+
+    Materialize.scrollFire(options);
+
+    $(function () {
+        $('.carousel.carousel-slider').carousel({
+            fullWidth: true,
+            indicators: false,
+        });
+    });
+
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+    //Open and closing the target
+
+    $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 300
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+    });
+
+});
